3e1bbb5b8a5e7cb3c3fd3eb62dff1ce21189d8fb
Dsnii <busai.dani@gmail.com>
2026-02-26 00:30:36 +0100

    test endpoint for get requests

---
+++ Project-Bloodwave-Backend/Controllers/TestController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Project_Bloodwave_Backend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TestController : ControllerBase
+    {
+        // GET /api/test/ping
+        [HttpGet("ping")]
+        public IActionResult Ping()
+        {
+            var result = new
+            {
+                ok = true,
+                message = "Bloodwave API is alive",
+                utc = DateTimeOffset.UtcNow,
+                server = Environment.MachineName,
+                random = Random.Shared.Next(1, 1_000_000)
+            };
+
+            return Ok(result);
+        }
+    }
+}
